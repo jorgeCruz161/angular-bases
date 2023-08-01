bcbc3da3cb6d8455368481b2883064d12abe2f47
jorgeCruz161 <jorge.ovando161@gmail.com>
2023-08-01 00:36:58 -0600

    first commit

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'She Hulk', 'Thor'];
+  public deletedHero?: string = '';
+
+  public removeLastHero(): void {
+    const deleteHero = this.heroNames.pop();
+
+    this.deletedHero = deleteHero;
+  }
+
+}
